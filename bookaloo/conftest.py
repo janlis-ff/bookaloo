9023868f77992f2311b9bcb87eff1769b4b0b536
Jan Lis <janlis@foxfactory.pl>
2025-04-23 13:32:47 +0200

    django-cookiecutter project init

---
+++ bookaloo/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from bookaloo.users.models import User
+from bookaloo.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
